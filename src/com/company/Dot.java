281f78116d70540cc5eba8a73cbcd0e4f8990725
Karlas <karolis.zagarnauskas@gmail.com>
2018-02-05 15:09:11 +0200

    2nd commit

---
+++ src/com/company/Dot.java
@@ -0,0 +1,85 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Dot {
+    Scanner sc = new Scanner(System.in);
+    public double x = 0;
+    public double y = 0;
+    public String spalva;
+
+    public Dot() {
+
+    }
+
+    public Dot(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Dot(int x, int y, String spalva) {
+        this.x = x;
+        this.y = y;
+        this.spalva = spalva;
+    }
+
+    public Dot(String spalva) {
+        this.spalva = spalva;
+    }
+
+
+    public double getDistance(Dot taskas) {
+        double result = 0;
+        //cia det formules skaiciavimui
+        //c=Kvadratine saknis is ((x2-x1)^2 + (y2-y1)^2)
+        result = Math.sqrt(Math.pow(this.x - taskas.x, 2) + Math.pow(this.y - taskas.y, 2));
+        return result;
+    }
+
+    public int getQuarter() {
+        int result = -1;
+        //cia deti furmules skaiciavima
+        if (this.x >= 0 && this.y >= 0) {
+            result = 0;
+        }
+        if (this.x > 0 && this.y < 0) {
+            result = 1;
+        }
+        if (this.x < 0 && this.y < 0) {
+            result = 2;
+        }
+        if (this.x < 0 && this.y >= 0) {
+            result = 3;
+        }
+
+        return result;
+    }
+
+    public boolean isSameQuarter(Dot taskas) {
+        boolean result = false;
+        result = this.getQuarter()==taskas.getQuarter();
+        //cia deti logika
+        return result;
+    }
+
+    public String toString() {
+        return "Spalva: " + this.spalva + " x: " + this.x + " y: " + this.y;
+    }
+
+    public boolean equals(Dot taskas) {
+        boolean result = this.getQuarter() == taskas.getQuarter();
+        if (this.x != taskas.x) {
+            result = false;
+        }
+        if (this.y != taskas.y) {
+            result = false;
+        }
+        if (this.spalva.equals(taskas.spalva)) {
+            result = false;
+        }
+
+        return result;
+    }
+}
+
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Dot taskas1;
+        Dot taskas2;
+        Dot taskas3;
+
+        taskas1 = new Dot(3, 4);
+        taskas2 = new Dot(-10, 10, "raudona");
+        taskas3 = new Dot("melyna");
+
+        System.out.println(taskas1);
+        System.out.println(taskas2);
+        System.out.println(taskas3);
+        System.out.println("atstumas tarp tasko 1 ir 3 "+ taskas1.getDistance(taskas3));
+        System.out.println("ar taspats ketvirtis tasko 1 ir 3 "+ taskas1.isSameQuarter(taskas3));
+        System.out.println("ar taspats ketvirtis tasko 1 ir 2 "+ taskas1.isSameQuarter(taskas2));
+
+        Dot taskas4 = new Dot(10,10,"raudona");
+        System.out.println("ar vienodi taskai 2 ir 4 "+taskas4.equals(taskas2));
     }
 }
